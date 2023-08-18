ec9c4b7d4f5b5f2259f4fcc00d7753ed4e239fdd
Matheus Marques <matheuscmf1995@gmail.com>
2023-08-18 08:36:56 -0300

    Primeiro Commit

---
+++ github_copilot/Teste.js
@@ -0,0 +1,9 @@
+{
+  teste0 - "Teste de inicialização"
+  teste1 ="Projeto para teste utilizando o GitHub Copilot"
+  teste2 ="Teste de commit"
+  teste3 ="Teste de push"
+  console.log(teste1, teste2, teste3)
+  teste4 = "Finalizado"
+  console.log(teste4)
+}
